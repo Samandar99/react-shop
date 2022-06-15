794bc9b7119fcf936f13903ff633fb7350487070
Samandar <laziz.go99@gmail.com>
2022-06-15 13:53:27 +0300

    gh-pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,10 @@ function Footer() {
         <div className="footer-copyright">
             <div className="container">
                 © {new Date().getFullYear()} Copyright Text
-                <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+                <a className="grey-text text-lighten-4 right" 
+                href="https://samandar99.github.io/react-shop" 
+                rel="noreferrer" 
+                target="_blank">Repo</a>
             </div>
         </div>
     </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ function Header() {
     return <nav className="green darken-1">
 
         <div className="nav-wrapper">
-            <a href="#" className="brand-logo">large Shop</a>
+            <a href="/" className="brand-logo">React Shop</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="#">Repo</a></li>
+                <li><a href="https://samandar99.github.io/react-shop" target='_blank' rel="noreferrer">Repo</a></li>
             </ul>
         </div>
 
