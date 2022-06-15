133c083cd2245836676a1f36d7b387853fd46f98
Samandar <laziz.go99@gmail.com>
2022-06-15 13:22:43 +0300

    consar

---
+++ src/components/BasketList.jsx
@@ -0,0 +1,49 @@
+import { BasketItem } from './BasketItem'
+
+
+function BasketList(props) {
+    const { order = [],
+        handleBaskShow = Function.prototype,
+        removeFromBasket = Function.prototype,
+        incQuantity,
+        decQuantity,
+
+    } = props;
+
+    const totalPrice = order.reduce((sum, el) => {
+        return sum + el.regularPrice * el.quantity
+    }, 0);
+
+    return <ul className="collection basket-list">
+
+        <li className="collection-item active">
+            Корзина
+        </li>
+        {
+            order.length ? order.map(item => (
+                <BasketItem
+                    key={item.mainId}
+                    removeFromBasket={removeFromBasket}
+                    incQuantity={incQuantity}
+                    decQuantity={decQuantity}
+                    {...item} />
+            )) : <li className='collection-item'> Корзина пуста </li>
+        }
+        <li className="collection-item active">
+            Общая стоимость: {totalPrice} rub
+
+        </li>
+
+        <li className="collection-item">
+            <button className='btn-small'>
+                Оформить
+            </button>
+        </li>
+
+
+        <i className='material-icons basket-close' onClick={handleBaskShow}>close</i>
+    </ul>
+
+}
+
+export { BasketList }
